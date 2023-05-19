ff63f8f4c3219911221ccbec754b0a66d8fade99
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-05-19 08:57:25 +0000

    Deploying to gh-pages from @ CodeOlga/goit-js-hw-09@deda2ad7bbe758aaac59f4f986081aef7b6a398a 🚀

--- 01-color-switcher.063e759b.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=null,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.setAttribute("disabled",""),n.removeAttribute("disabled","")})),n.addEventListener("click",(function(){clearInterval(t),e.removeAttribute("disabled",""),n.setAttribute("disabled","")})),n.setAttribute("disabled","")}();
-//# sourceMappingURL=01-color-switcher.063e759b.js.map
