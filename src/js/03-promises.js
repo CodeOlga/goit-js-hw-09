22431ac59e4b548f420e05606ac0932ceb2fdf50
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-05-21 18:27:27 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,13 +11,40 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  const firstDelay = e.currentTarget.delay;
-  const delayStep = e.currentTarget.step;
-  const amount = e.currentTarget.amount;
-  // const {
-  //   elements: { amount, delay, step },
-  // } = e.currentTarget;
-  console.log(e);
+
+  // const delay = e.currentTarget.delay;
+  // const step = e.currentTarget.step;
+  // const amount = e.currentTarget.amount;
+
+  const {
+    elements: { amount, delay, step },
+  } = e.currentTarget;
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    // step + 1;
+    let totalDelay = parseInt(delay.value) + parseInt(step.value);
+    // const totalDelay = parseInt(delay.value) + parseInt(step.value);
+    totalDelay += parseInt(step.value);
+    // console.log(totalDelay);
+    createPromise(i, totalDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+  // console.log(e);
+  // console.log(totalDelay);
+  // console.log(delay);
+  // console.log(e.currentTarget.step);
+  // console.log(e.currentTarget.amount);
+
+  // return;
 }
 
 // const createPromise = (position, delay) => {
@@ -25,24 +52,24 @@ function onFormSubmit(e) {
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    // setTimeout(() => {
-    if (shouldResolve) {
-      // resolve(onFormSubmit);
-      resolve({ position, delay });
-      // resolve({ firstDelay, delayStep, amount });
-      // resolve('Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)');
-    } else {
-      reject();
-      // reject('Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)');
-    }
-    // }, delay);
+    setTimeout(() => {
+      if (shouldResolve) {
+        // resolve(onFormSubmit);
+        resolve({ position, delay });
+        // resolve({ firstDelay, delayStep, amount });
+        // resolve('Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)');
+      } else {
+        reject({ position, delay });
+        // reject('Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)');
+      }
+    }, delay);
   });
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
